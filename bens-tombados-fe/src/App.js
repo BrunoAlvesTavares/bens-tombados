d7f50877338f06db410c21cca39ef7afc159ab8c
rafalvesc <racosta@inf.ufsm.br>
2024-12-04 02:50:11 -0300

    feat: edited home page

--- bens-tombados-fe/src/App.js
+++ bens-tombados-fe/src/App.js
@@ -1,4 +1,3 @@
-// src/App.js
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'; // Importe as funcionalidades de roteamento
@@ -11,30 +10,72 @@ import Categoria from './pages/categoria/categoria';
 import Classe from './pages/classe/classe';
 import SubClasse from './pages/subclasse/subclasse';
 import Processo from './pages/processo/processo';
+import Paper from '@mui/material/Paper'; // Importando o Paper do Material UI
+import Typography from '@mui/material/Typography'; // Importando o Typography para texto
 
 function App() {
   return (
-    <Router> {/* Envolva o App com Router para ativar o roteamento */}
+    <Router>
       <div className="App">
-        <Header title="Bens tombados" /> {/* Apenas o Header */}
+        <Header title="Bens tombados" />
         
         {/* Defina as rotas do seu aplicativo */}
         <Routes>
           {/* A rota "/" renderiza a página inicial */}
-          <Route path="/" element={<div> 
-            <h2>Bem-vindo à página principal</h2>
-            <p>Esta é a página inicial.</p>
-          </div>} />
+          <Route 
+            path="/" 
+            element={
+              <div> 
+                <Paper 
+                  elevation={3} 
+                  style={{ 
+                    padding: '20px', 
+                    margin: '20px', 
+                    width: '80%',  // Definindo a largura do Paper para 80% da tela
+                    maxWidth: '1200px', // Definindo a largura máxima
+                    marginLeft: 'auto', // Centralizando o Paper
+                    marginRight: 'auto' // Centralizando o Paper
+                  }}
+                >
+                  <Typography variant="h4" component="h2" gutterBottom>
+                    Fundamentos de banco de dados
+                  </Typography>
+                  <Typography variant="body1" paragraph>
+                    Rafael Alves e Bruno Alves
+                  </Typography>
+                  <Typography variant="body1" paragraph>
+                    O banco de dados do patrimônio cultural tombado pelo Estado de Minas Gerais diz respeito aos bens culturais materiais que, em função de seu valor histórico, artístico, estético, afetivo, simbólico, dentre outros, receberam proteção pelo poder público estadual pelo instrumento do tombamento. Um bem cultural tombado encontra-se sob um regime especial de tutela pelo Estado, uma vez que a ele foi atribuído um valor social. É importante destacar que os bens tombados em âmbito estadual não podem ser destruídos, demolidos, mutilados ou sofrer intervenções sem ciência, análise prévia, aprovação e acompanhamento técnico do Instituto Estadual do Patrimônio Histórico e Artístico de Minas Gerais - Iepha-MG.
+                  </Typography>
+                  <Typography variant="body1" paragraph>
+                    O Iepha-MG é o responsável por promover a identificação, o reconhecimento e a salvaguarda do patrimônio cultural do Estado, sendo o tombamento uma das medidas administrativas para a proteção desse patrimônio. O tombamento é instituído por um ato legal e, posteriormente, o bem tombado é inscrito como integrante do patrimônio cultural de Minas Gerais em livro(s) de tombo estabelecidos pelo Decreto Estadual nº 14.260, de 14/01/1972. No Iepha-MG, a Gerência de Patrimônio Cultural Material é responsável por realizar as ações de proteção dos bens culturais materiais e a Gerência de Identificação e Pesquisa tem como uma de suas competências reunir e manter atualizadas as informações sobre os bens tombados (Decreto nº 47.921, de 22/04/2020)
+                  </Typography>
+                  <Typography variant="body1" paragraph>
+                    [1.0.0] - 2023-08-30<br />
+                    Versão inicial publicada.
+                  </Typography>
+                  <Typography variant="body1" paragraph>
+                    Licença: CC-BY-4.0<br />
+                    Formatos: CSV; JSON;<br />
+                    Atualização: -<br />
+                    Última alteração: 12/10/2024 05:38:24
+                  </Typography>
+                  <Typography variant="body1" paragraph>
+                    <a href="https://dados.gov.br/dados/conjuntos-dados/bens-tombados2" target="_blank" rel="noopener noreferrer">
+                      Acesse o repositório
+                    </a>
+                  </Typography>
+                </Paper>
+              </div>
+            } 
+          />
 
-          {/* A rota "/livro-tombo" renderiza a página Livro Tombo */}
+          {/* As demais rotas */}
           <Route path="/livro-tombo" element={<LivroTombo />} />
-
-          {/* A rota "/ato-legal" renderiza a página Ato Legal */}
           <Route path="/ato-legal" element={<AtoLegal />} />
           <Route path="/distrito" element={<Distrito />} />
           <Route path="/municipio" element={<Municipio />} />
           <Route path="/categoria" element={<Categoria />} />
-          <Route path="/classe" element={<Classe/>} />
+          <Route path="/classe" element={<Classe />} />
           <Route path="/subclasse" element={<SubClasse />} />
           <Route path="/processo" element={<Processo />} />
         </Routes>
