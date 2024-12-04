c04209d1d5c33df6325fc6d626210838d4f893e7
BrunoAlvesTavares <brunoalvestavares@hotmail.com>
2024-12-04 03:15:22 -0300

    chore: created book

--- bens-tombados-fe/src/pages/livro-tombo/adicionar-livro.js
+++ bens-tombados-fe/src/pages/livro-tombo/adicionar-livro.js
@@ -7,22 +7,19 @@ const API_URL = 'http://localhost:3000/livros-tombo';
 
 export default function AdicionarLivro() {
   const [nomeLivro, setNomeLivro] = useState('');
-  const [idLivro, setIdLivro] = useState('');
   const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    // Dados do livro
     const newLivro = {
-      idLivro: idLivro,
       nomeLivro: nomeLivro,
     };
 
     try {
       await axios.post(API_URL, newLivro);
       alert('Livro adicionado com sucesso!');
-      navigate('/'); // Redireciona de volta para a lista de livros
+      navigate('/');
     } catch (error) {
       console.error('Erro ao adicionar o livro:', error);
       alert('Erro ao adicionar o livro.');
@@ -42,40 +39,13 @@ export default function AdicionarLivro() {
         backgroundColor: '#f5f5f5',
         boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
         borderRadius: 2,
-        marginTop: 5
+        marginTop: 5,
       }}
     >
       <Typography variant="h5" sx={{ mb: 3 }}>
         Adicionar Novo Livro
       </Typography>
       <form onSubmit={handleSubmit} style={{ width: '100%' }}>
-        <TextField
-          label="ID do Livro"
-          fullWidth
-          value={idLivro}
-          onChange={(e) => setIdLivro(e.target.value)}
-          required
-          sx={{
-            mb: 2,
-            '& .MuiOutlinedInput-root': {
-              '& fieldset': {
-                borderColor: '#BDBDBD', // Borda cinza por padrão
-              },
-              '&:hover fieldset': {
-                borderColor: '#BDBDBD', // Cor da borda ao passar o mouse (cinza)
-              },
-              '&.Mui-focused fieldset': {
-                borderColor: '#D50032', // Cor da borda quando estiver em foco (vermelho)
-              },
-            },
-            '& .MuiInputLabel-root': {
-              color: '#BDBDBD', // Cor do label cinza por padrão
-            },
-            '& .MuiInputLabel-root.Mui-focused': {
-              color: '#D50032', // Cor do label ao focar (vermelho)
-            },
-          }}
-        />
         <TextField
           label="Nome do Livro"
           fullWidth
@@ -86,20 +56,20 @@ export default function AdicionarLivro() {
             mb: 3,
             '& .MuiOutlinedInput-root': {
               '& fieldset': {
-                borderColor: '#BDBDBD', // Borda cinza por padrão
+                borderColor: '#BDBDBD', 
               },
               '&:hover fieldset': {
-                borderColor: '#BDBDBD', // Cor da borda ao passar o mouse (cinza)
+                borderColor: '#BDBDBD',
               },
               '&.Mui-focused fieldset': {
-                borderColor: '#D50032', // Cor da borda quando estiver em foco (vermelho)
+                borderColor: '#D50032', 
               },
             },
             '& .MuiInputLabel-root': {
-              color: '#BDBDBD', // Cor do label cinza por padrão
+              color: '#BDBDBD',
             },
             '& .MuiInputLabel-root.Mui-focused': {
-              color: '#D50032', // Cor do label ao focar (vermelho)
+              color: '#D50032', 
             },
           }}
         />
